32b65c415e2dd66d90a27e0f77a457df096a6898
Joshua Worden <solocommand@gmail.com>
2010-10-15 02:12:29 +0000

    realid working, starting on friends/guild/alttracker

--- TitanSocial.lua
+++ TitanSocial.lua
@@ -57,6 +57,7 @@ function TitanPanelSocialButton_OnLoad(self)
 		bRealID = true;			-- Enable/disable RealID Friends Component
 		bGuild = true;			-- Enable/disable Guild Component
 		bButtonLabel = true;	-- Enable/disable static label text
+		bButtonIcon = false;	-- Enable/disable static button icon
 		
 		-- Dynamic Settings
 		bGuildOffline = GetGuildRosterShowOffline()	-- Enable/disable including offline guild members
@@ -118,51 +119,51 @@ end
 ----------------------------------------------------------------------
 
 function TitanPanelSocialButton_GetButtonText(id)
-
-	--
-	-- Get Social Online
-	--
+	local id = TitanUtils_GetButton(id);
+	local iRealIDTotal, iRealIDOnline = BNGetNumFriends();
+	local iFriendsTotal, iFriendsOnline = GetNumFriends();
+	local iGuildTotal, iGuildOnline = GetNumGuildMembers();
+	local tButtonRichText = "";
+	local tButtonLabel = "";
 	
-		-- RealID
-		iRealIDTotal, iRealIDOnline = BNGetNumFriends();
-		
-		-- Friends
-		iFriendsOnline = GetNumFriends();
-		
-		-- Guild
-		iGuildTotal = GetNumGuildMembers(true);
-		iGuildOnline = GetNumGuildMembers;
-		
+	TITAN_SOCIAL_BUTTON_LABEL = "Social: ";
+	TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL.."%s"..TitanUtils_GetNormalText("/").."%s"..TitanUtils_GetNormalText("/").."%s";
+
+	tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, "|cff00A2E8"..iRealIDOnline.."|r", TitanUtils_GetNormalText(iFriendsOnline), "|cff00FF00"..iGuildOnline.."|r");
+	
 	--
 	-- Label Generation
 	--
-
-		-- Button Label
-		if(bButtonLabel) then
-			tButtonLabel = "Social: "
-		else
-			tButtonLabel = ""
-		end
-		
-		tButtonRichText = "";
-		
-		-- RealID
-		if(bRealID) then
-			tButtonRichText = tButtonRichText.."|cff00A2E8"..iRealIDOnline.." ".._G["FONT_COLOR_CODE_CLOSE"];
-		end
-		
-		-- Friends
-		if(bFriends) then
-			tButtonRichText = tButtonRichText..iFriendsOnline.." ";
-		end
-		
-		-- Guild
-		if(bGuild) then
-			tButtonRichText = tButtonRichText.."|cff00FF00"..iGuildOnline.._G["FONT_COLOR_CODE_CLOSE"];
-		end
+	--
+	--	-- Button Label
+	--	if(bButtonLabel) then
+	--		tButtonLabel = "Social: ";
+	--	end
+	--		
+	--	-- RealID
+	--	if(bRealID) then
+	--		tButtonRichText = tButtonRichText.."|cff00A2E8"..iRealIDOnline.."|r";
+	--	end
+	--	
+	--	-- Friends
+	--	if(bFriends) then
+	--		if(not bRealID) then
+	--			tButtonRichText = tButtonRichText..iFriendsOnline.." ";
+	--		else
+	--			tButtonRichText = tButtonRichText..TitanUtils_GetNormalText(" / ")..iFriendsOnline.." ";
+	--		end
+	--	end
+	--	
+	--	-- Guild
+	--	if(bGuild) then
+	--		if(not bFriends) and (not bRealID) then
+	--			tButtonRichText = tButtonRichText.."|cff00FF00"..iGuildOnline.."|r";
+	--		elseif(not bFriends) then
+	--			tButtonRichText = tButtonRichText..TitanUtils_GetNormalText(" / ").."|cff00FF00"..iGuildOnline.."|r";
+	--		end
+	--	end
 	
-	
-	return tButtonLabel, tButtonRichText
+	return TITAN_SOCIAL_BUTTON_LABEL, tButtonRichText;
 end
 
 ----------------------------------------------------------------------
@@ -171,6 +172,145 @@ end
 
 function TitanPanelSocialButton_GetTooltipText()
 
+	local iRealIDTotal, iRealIDOnline = BNGetNumFriends();
+	local iFriendsTotal, iFriendsOnline = GetNumFriends();
+	local iGuildTotal, iGuildOnline = GetNumGuildMembers();
+	local tTooltipRichText, playerStatus, clientName = "";
+		
+	--
+	-- Tooltip Header
+	--
+	--	tTooltipRichText = tTooltipRichText..TitanUtils_GetNormalText("RealID Friends Online:").."\t".."|cff00A2E8"..iRealIDOnline.."|r"..TitanUtils_GetNormalText("/"..iRealIDTotal).."\n"
+	--	tTooltipRichText = tTooltipRichText..TitanUtils_GetNormalText("Normal Friends Online:").."\t".."|cffFFFFFF"..iFriendsOnline.."|r"..TitanUtils_GetNormalText("/"..iFriendsTotal).."\n"
+	--	tTooltipRichText = tTooltipRichText..TitanUtils_GetNormalText("Guild Members Online:").."\t".."|cff00FF00"..iGuildOnline.."|r"..TitanUtils_GetNormalText("/"..iGuildTotal).."\n"
+	--	tTooltipRichText = tTooltipRichText.." ".."\n"	-- Line Break
+	
+	--
+	-- RealID Friends
+	--
+	
+		tTooltipRichText = tTooltipRichText..TitanUtils_GetNormalText("RealID Friends Online:").."\t".."|cff00A2E8"..iRealIDOnline.."|r"..TitanUtils_GetNormalText("/"..iRealIDTotal).."\n"
+		
+		for friendIndex=1, iRealIDOnline do
+
+			presenceID, givenName, surname, toonName, toonID, client, isOnline, lastOnline, isAFK, isDND, messageText, noteText, isFriend, unknown = BNGetFriendInfo(friendIndex)
+			unknowntoon, toonName, client, realmName, faction, race, class, unknown, zoneName, level, gameText, broadcastText, broadcastTime = BNGetToonInfo(presenceID)
+
+			-- playerStatus
+				if (isAFK) then
+					playerStatus = "AFK"
+				elseif (isDND) then
+					playerStatus = "DND"
+				else
+					playerStatus = ""
+				end
+			
+			-- Class Colors
+				if (class == "Druid") then
+					classColor = "|cffff7d0a";
+				elseif (class == "Hunter") then
+					classColor = "|cffabd473";
+				elseif (class == "Mage") then
+					classColor = "|cff69ccf0";
+				elseif (class == "Paladin") then
+					classColor = "|cfff58cba";
+				elseif (class == "Priest") then
+					classColor = "|cffffffff";
+				elseif (class == "Rogue") then
+					classColor = "|cfffff569";
+				elseif (class == "Shaman") then
+					classColor = "|cff2459ff";
+				elseif (class == "Warlock") then
+					classColor = "|cff9482ca";
+				elseif (class == "Warrior") then
+					classColor = "|cffc79c6e";
+				elseif (class == "Death Knight") then
+					classColor = "|cffc41f3b";
+				else
+					classColor = "|cff000000";
+				end
+			
+			-- Stan Smith {SC2} ToonName 80 <AFK/DND>\t Location
+			-- Stan Smith Toonname 80
+			
+			-- Full Name
+			tTooltipRichText = tTooltipRichText.."|cff00A2E8"..givenName.." "..surname.."|r "
+			
+			-- Game Name
+			--tTooltipRichText = tTooltipRichText.."{"..client.."} "
+
+			-- Character
+			tTooltipRichText = tTooltipRichText..classColor..toonName.."|r "
+				
+			-- Character Level
+			tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..level.."|r "
+						
+			-- Status
+			if (playerStatus ~= "") then
+				tTooltipRichText = tTooltipRichText.."<"..playerStatus..">"
+			end
+			
+			-- Character Location
+			tTooltipRichText = tTooltipRichText.."\t|cffFFFFFF"..gameText.."|r\n";
+
+			-- Broadcast
+			--if (broadcastText ~= "") then
+			--	tTooltipRichText = tTooltipRichText.." \t|cff00A0E0"..broadcastText.."|r\n";
+			--end		
+		
+		end
+	
+	--
+	-- Friends
+	--
+		
+		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText("Normal Friends Online:").."\t".."|cffFFFFFF"..iFriendsOnline.."|r"..TitanUtils_GetNormalText("/"..iFriendsTotal).."\n"
+		
+	--
+	-- Guild
+	--
+		
+		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText("Guild Members Online:").."\t".."|cff00FF00"..iGuildOnline.."|r"..TitanUtils_GetNormalText("/"..iGuildTotal).."\n"
+	
+	return tTooltipRichText;
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
