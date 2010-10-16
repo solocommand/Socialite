daf5d6c39ce2a17fe8607ab82a501beb177d0ce6
solocommand <solocommand@71d52551-c826-4352-a584-67b5f1798420>
2010-10-16 23:16:01 +0000

    Tagging as 4.0.1r13-release

---
+++ tags/4.0.1r13-release/TitanSocial.lua
@@ -0,0 +1,479 @@
+
+----------------------------------------------------------------------
+--  Local variables
+----------------------------------------------------------------------
+
+-- Debugging Mode
+	bDebugMode = false;
+
+-- Required Titan variables
+	TITAN_SOCIAL_ID = "Social";
+	TITAN_SOCIAL_VERSION = "4.0.1r13-release";
+	TITAN_NIL = false;
+
+-- Friend-specific variables
+	iFriendsTab = 1;
+-- RealID-specific variables
+-- Guild-specific variables
+	iGuildTab = 1;
+
+-- Counters for Titan Bar Display
+	iRealIDOnline, iFriendsOnline, iGuildOnline = 0;
+
+----------------------------------------------------------------------
+--  Global variables
+----------------------------------------------------------------------
+
+
+
+----------------------------------------------------------------------
+--  TitanPanelSocialButton_OnLoad(self)
+----------------------------------------------------------------------
+
+function TitanPanelSocialButton_OnLoad(self)
+
+	--
+	-- LOCAL REGISTRY --
+	--
+	
+		self.registry = { 
+			id = TITAN_SOCIAL_ID,
+			version = TITAN_SOCIAL_VERSION,
+			menuText = TITAN_SOCIAL_MENU_TEXT, 
+			buttonTextFunction = "TitanPanelSocialButton_GetButtonText",
+			tooltipTitle = TITAN_SOCIAL_TOOLTIP,
+			tooltipTextFunction = "TitanPanelSocialButton_GetTooltipText",
+			iconWidth = 16,
+			icon = "Interface\\FriendsFrame\\BroadcastIcon";
+			savedVariables = {       
+				ShowRealID = 1,
+				ShowFriends = 1,
+				ShowGuild = 1,
+				ShowLabel = 1,
+				ShowIcon = 1,
+			  }
+		};
+
+	--
+	-- CONFIGURATION --
+	--
+	
+		-- Load these settings from SavedVariables/&Set Defaults
+		
+		-- Dynamic Settings
+		bGuildOffline = GetGuildRosterShowOffline()	-- Enable/disable including offline guild members
+
+	--
+	-- EVENT CATCHING --
+	--
+	
+		-- General Events
+		self:RegisterEvent("PLAYER_ENTERING_WORLD");
+		
+		-- RealID Events
+		self:RegisterEvent("BN_FRIEND_ACCOUNT_OFFLINE");
+		self:RegisterEvent("BN_FRIEND_ACCOUNT_ONLINE");
+		self:RegisterEvent("BN_FRIEND_TOON_OFFLINE");
+		self:RegisterEvent("BN_FRIEND_TOON_ONLINE");
+		self:RegisterEvent("BN_TOON_NAME_UPDATED");
+		
+		-- Friend Events
+		self:RegisterEvent("FRIENDLIST_UPDATE");
+		
+		-- Guild Events
+		self:RegisterEvent("GUILD_ROSTER_UPDATE");
+		
+end
+
+----------------------------------------------------------------------
+--  TitanPanelSocialButton_OnEvent(self, event, ...)
+----------------------------------------------------------------------
+
+function TitanPanelSocialButton_OnEvent(self, event, ...)
+
+	if(bDebugMode) then
+		if(event == "PLAYER_ENTERING_WORLD") then
+			DEFAULT_CHAT_FRAME:AddMessage(TITAN_SOCIAL_ID.." v"..TITAN_SOCIAL_VERSION.." Loaded.");
+		end
+		DEFAULT_CHAT_FRAME:AddMessage("Social: Caught Event "..event);
+	end
+
+	TitanPanelButton_UpdateButton(TITAN_SOCIAL_ID);
+
+end
+
+----------------------------------------------------------------------
+--  TitanPanelSocialButton_OnUpdate(self, elapsed)
+----------------------------------------------------------------------
+
+function TitanPanelSocialButton_OnUpdate(self, elapsed)
+
+	TitanPanelSocialButton_GetButtonText(TitanUtils_GetButton(id));
+
+end
+
+----------------------------------------------------------------------
+--  TitanPanelSocialButton_OnClick(self, button)
+----------------------------------------------------------------------
+
+function TitanPanelSocialButton_OnClick(self, button)
+
+	if (button == "LeftButton") then
+		if (not FriendsFrame:IsVisible()) then
+			ToggleFriendsFrame(iFriendsTab);
+			FriendsFrame_Update();
+			ToggleGuildFrame(iGuildTab);
+		elseif (FriendsFrame:IsVisible()) then
+			ToggleFriendsFrame(iFriendsTab);
+			ToggleGuildFrame(iGuildTab);
+		end
+	end
+	
+end
+
+----------------------------------------------------------------------
+--  TitanPanelRightClickMenu_PrepareSocialMenu()
+----------------------------------------------------------------------
+
+function TitanPanelRightClickMenu_PrepareSocialMenu()     
+  
+    local info = {};  
+    TitanPanelRightClickMenu_AddTitle(TitanPlugins[TITAN_SOCIAL_ID].menuText);
+    TitanPanelRightClickMenu_AddToggleVar("Show RealID Friends", TITAN_SOCIAL_ID, "ShowRealID");
+	TitanPanelRightClickMenu_AddToggleVar("Show Friends", TITAN_SOCIAL_ID, "ShowFriends");
+	TitanPanelRightClickMenu_AddToggleVar("Show Guild Members", TITAN_SOCIAL_ID, "ShowGuild");
+    TitanPanelRightClickMenu_AddSpacer();
+    TitanPanelRightClickMenu_AddToggleIcon(TITAN_SOCIAL_ID);
+	TitanPanelRightClickMenu_AddToggleVar("Show Label", TITAN_SOCIAL_ID, "ShowLabel");
+    TitanPanelRightClickMenu_AddSpacer();
+    TitanPanelRightClickMenu_AddCommand("Hide", TITAN_SOCIAL_ID, TITAN_PANEL_MENU_FUNC_HIDE);
+  
+end
+
+----------------------------------------------------------------------
+-- TitanPanelSocialButton_GetButtonText(id)
+----------------------------------------------------------------------
+
+function TitanPanelSocialButton_GetButtonText(id)
+	local id = TitanUtils_GetButton(id);
+	local iRealIDTotal, iRealIDOnline = 0;
+	local iFriendsTotal, iFriendsOnline = 0;
+	local iGuildTotal, iGuildOnline = 0;
+	local tButtonRichText = "";
+	
+	local tButtonTemplate1 = "%s";
+	local tButtonTemplate2 = "%s |cffffd200/|r %s";
+	local tButtonTemplate3 = "%s |cffffd200/|r %s |cffffd200/|r %s";
+
+	--
+	-- SavedVars//Colors
+	--
+	if (TitanGetVar(TITAN_SOCIAL_ID, "ShowRealID") ~= nil) then
+		iRealIDTotal, iRealIDOnline = BNGetNumFriends();
+		iRealIDOnline  = "|cff00A2E8"..iRealIDOnline.."|r";
+	end
+	
+	if (TitanGetVar(TITAN_SOCIAL_ID, "ShowFriends") ~= nil) then
+		iFriendsTotal, iFriendsOnline = GetNumFriends();
+		iFriendsOnline = "|cffFFFFFF"..iFriendsOnline.."|r";
+	end
+	
+	if (TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~= nil) then
+		iGuildTotal, iGuildOnline = GetNumGuildMembers();
+		iGuildOnline   = "|cff00FF00"..iGuildOnline.."|r";
+	end
+	
+	if(TitanGetVar(TITAN_SOCIAL_ID, "ShowLabel") ~= nil) then
+		TITAN_SOCIAL_BUTTON_LABEL = "Social: ";
+	else
+		TITAN_SOCIAL_BUTTON_LABEL = " ";
+	end
+	
+	--
+	-- Custom Labels --
+	--
+	
+	if (TitanGetVar(TITAN_SOCIAL_ID, "ShowRealID") ~= nil) then
+		if (TitanGetVar(TITAN_SOCIAL_ID, "ShowFriends") ~= nil) then
+			if (TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~= nil) then
+				-- ## / ## / ## Ok
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate3;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iRealIDOnline, iFriendsOnline, iGuildOnline);
+			else
+				-- ## / ## / XX Ok
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate2;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iRealIDOnline, iFriendsOnline);
+			end
+		else
+			if (TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~= nil) then
+				-- ## / XX / ## Ok
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate2;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iRealIDOnline, iGuildOnline);
+			else
+				-- ## / XX / XX Ok
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate1;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iRealIDOnline);
+			end
+		end
+	else
+		if (TitanGetVar(TITAN_SOCIAL_ID, "ShowFriends") ~= nil) then
+			if (TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~= nil) then
+				-- XX / ## / ## Ok
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate2;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iFriendsOnline, iGuildOnline);
+			else
+				-- XX / ## / XX
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate1;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iFriendsOnline);
+			end
+		else
+			if (TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~= nil) then
+				-- XX / XX / ##
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate1;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, iGuildOnline);
+			else
+				-- XX / XX / XX
+				TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_LABEL..tButtonTemplate1;
+				tButtonRichText = format(TITAN_SOCIAL_BUTTON_TEXT, "Disabled");
+			end
+		end
+	end
+
+	return TITAN_SOCIAL_BUTTON_LABEL, tButtonRichText;
+end
+
+----------------------------------------------------------------------
+-- TitanPanelSocialButton_GetTooltipText()
+----------------------------------------------------------------------
+
+function TitanPanelSocialButton_GetTooltipText()
+
+	local iRealIDTotal, iRealIDOnline = 0;
+	local iFriendsTotal, iFriendsOnline = 0;
+	local iGuildTotal, iGuildOnline = 0;
+	local tTooltipRichText, playerStatus, clientName = "";
+	
+	--
+	--	RealID Friends
+	--
+	
+	if(TitanGetVar(TITAN_SOCIAL_ID, "ShowRealID") ~=nil) then
+		iRealIDTotal, iRealIDOnline = BNGetNumFriends();
+		--iRealIDOnline  = "|cff00A2E8"..iRealIDOnline.."|r";
+
+		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText("RealID Friends Online:").."\t".."|cff00A2E8"..iRealIDOnline.."|r"..TitanUtils_GetNormalText("/"..iRealIDTotal).."\n"
+		
+		for friendIndex=1, iRealIDOnline do
+
+			presenceID, givenName, surname, toonName, toonID, client, isOnline, lastOnline, isAFK, isDND, messageText, noteText, isFriend, unknown = BNGetFriendInfo(friendIndex)
+			unknowntoon, toonName, client, realmName, faction, race, class, unknown, zoneName, level, gameText, broadcastText, broadcastTime = BNGetToonInfo(presenceID)
+
+			-- playerStatus
+				if (isAFK) then
+					playerStatus = "AFK"
+				elseif (isDND) then
+					playerStatus = "DND"
+				else
+					playerStatus = ""
+				end
+				
+			-- Client Information
+				if (client == "S2") then
+					clientName = "S2"
+				elseif (client == "D3") then
+					clientName = "D3"
+				else
+					clientName = "??"
+				end
+			
+			-- Class Colors
+				if (class == "Druid") then				classColor = "|cffff7d0a";
+				elseif (class == "Hunter") then			classColor = "|cffabd473";
+				elseif (class == "Mage") then			classColor = "|cff69ccf0";
+				elseif (class == "Paladin") then		classColor = "|cfff58cba";
+				elseif (class == "Priest") then			classColor = "|cffffffff";
+				elseif (class == "Rogue") then			classColor = "|cfffff569";
+				elseif (class == "Shaman") then			classColor = "|cff2459ff";
+				elseif (class == "Warlock") then		classColor = "|cff9482ca";
+				elseif (class == "Warrior") then		classColor = "|cffc79c6e";
+				elseif (class == "Death Knight") then	classColor = "|cffc41f3b";
+				else									classColor = "|cffCCCCCC";
+				end
+			
+			-- Stan Smith {SC2} ToonName 80 <AFK/DND>\t Location
+			-- Stan Smith Toonname 80 (SC2)
+			
+			if(client ~= "WoW") then
+				-- Client Name
+				tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..clientName.."|r  "
+			else
+				-- Character Level
+				tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..level.."|r  "
+			end
+			
+			-- Character
+			tTooltipRichText = tTooltipRichText..classColor..toonName.."|r  "
+			
+			-- Full Name
+			tTooltipRichText = tTooltipRichText.."[|cff00A2E8"..givenName.." "..surname.."|r]  "
+			
+			-- Status
+			if (playerStatus ~= "") then
+				tTooltipRichText = tTooltipRichText.."<"..playerStatus..">"
+			end
+			
+			-- Character Location
+			tTooltipRichText = tTooltipRichText.."\t|cffFFFFFF"..gameText.."|r\n";
+
+			-- Broadcast
+			--if (broadcastText ~= "") then
+			--	tTooltipRichText = tTooltipRichText.." \t|cff00A0E0"..broadcastText.."|r\n";
+			--end		
+		
+		end
+	end
+	
+	--
+	-- Friends
+	--
+	
+	if(TitanGetVar(TITAN_SOCIAL_ID, "ShowFriends") ~=nil) then
+	
+		iFriendsTotal, iFriendsOnline = GetNumFriends();
+	
+		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText("Normal Friends Online:").."\t".."|cffFFFFFF"..iFriendsOnline.."|r"..TitanUtils_GetNormalText("/"..iFriendsTotal).."\n"
+		
+		for friendIndex=1, iFriendsOnline do
+		
+			name, level, class, area, connected, playerStatus, playerNote, RAF = GetFriendInfo(friendIndex);
+
+			-- Class Colors
+				if (class == "Druid") then				classColor = "|cffff7d0a";
+				elseif (class == "Hunter") then			classColor = "|cffabd473";
+				elseif (class == "Mage") then			classColor = "|cff69ccf0";
+				elseif (class == "Paladin") then		classColor = "|cfff58cba";
+				elseif (class == "Priest") then			classColor = "|cffffffff";
+				elseif (class == "Rogue") then			classColor = "|cfffff569";
+				elseif (class == "Shaman") then			classColor = "|cff2459ff";
+				elseif (class == "Warlock") then		classColor = "|cff9482ca";
+				elseif (class == "Warrior") then		classColor = "|cffc79c6e";
+				elseif (class == "Death Knight") then	classColor = "|cffc41f3b";
+				else									classColor = "|cffCCCCCC";
+				end
+			
+			-- Level
+			--tToolTipRichText = tTooltipRichText.."|cffFFFFFF##"..level.."|r  ";
+			tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..level.."|r  ";
+			
+			-- Name
+			tTooltipRichText = tTooltipRichText..classColor..name.."|r ";
+
+			-- Status
+			if (playerStatus ~= "") then
+				tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..playerStatus.."|r ";
+			end
+			
+			-- Notes
+			--if(playerNote ~= "") then
+			--	tTooltipRichText = tTooltipRichText..playerNote;
+			--end
+			
+			-- Location
+			tTooltipRichText = tTooltipRichText.."\t|cffFFFFFF"..area.."|r\n";
+		
+		end
+	end
+	
+	--
+	-- Guild
+	--
+	
+	if(TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~=nil) then
+	
+		iGuildTotal, iGuildOnline = GetNumGuildMembers();
+		--iGuildOnline   = "|cff00FF00"..iGuildOnline.."|r";
+		
+		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText("Guild Members Online:").."\t".."|cff00FF00"..iGuildOnline.."|r"..TitanUtils_GetNormalText("/"..iGuildTotal).."\n"
+		
+		for guildIndex=1, iGuildOnline do
+		
+			name, rank, rankIndex, level, class, zone, note, officernote, online, playerStatus, classFileName = GetGuildRosterInfo(guildIndex);
+			
+			-- Class Colors
+				if (class == "Druid") then				classColor = "|cffff7d0a";
+				elseif (class == "Hunter") then			classColor = "|cffabd473";
+				elseif (class == "Mage") then			classColor = "|cff69ccf0";
+				elseif (class == "Paladin") then		classColor = "|cfff58cba";
+				elseif (class == "Priest") then			classColor = "|cffffffff";
+				elseif (class == "Rogue") then			classColor = "|cfffff569";
+				elseif (class == "Shaman") then			classColor = "|cff2459ff";
+				elseif (class == "Warlock") then		classColor = "|cff9482ca";
+				elseif (class == "Warrior") then		classColor = "|cffc79c6e";
+				elseif (class == "Death Knight") then	classColor = "|cffc41f3b";
+				else									classColor = "|cffCCCCCC";
+				end
+		
+			-- 80 {color=class::Playername} {<AFK>} Rank Note ONote\t Location
+			
+			-- Level
+			tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..level.."|r  "
+			-- Name
+			tTooltipRichText = tTooltipRichText..classColor..name.."|r  "
+			-- Status
+			if (playerStatus ~= "") then
+				tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..playerStatus.."|r  ";
+			end
+			-- Rank
+			tTooltipRichText = tTooltipRichText..rank.."  ";
+			-- Notes
+			tTooltipRichText = tTooltipRichText.."|cffFFFFFF"..note.."|r  "
+			tTooltipRichText = tTooltipRichText.."|cffAAFFAA"..officernote.."|r  "
+			-- Location
+			tTooltipRichText = tTooltipRichText.."\t|cffFFFFFF"..zone.."|r\n"			
+			
+		end
+	
+	end
+	
+	return tTooltipRichText;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tags/4.0.1r13-release/localization.lua
@@ -0,0 +1,11 @@
+﻿local L = LibStub("AceLocale-3.0"):GetLocale("Titan", true)
+
+-- default (EN)
+
+TITAN_SOCIAL_MENU_TEXT = "Social"
+
+TITAN_SOCIAL_BUTTON_LABEL = "Social: "
+TITAN_SOCIAL_BUTTON_TEXT = "Social: "
+TITAN_SOCIAL_TOOLTIP = "Social Information"
+
+-- TITAN_SOCIAL_BUTTON_TEXT = TITAN_SOCIAL_BUTTON_TEXT.."%s"..TitanUtils_GetHighlightText("/").."%s"..TitanUtils_GetHighlightText("/").."%s";
