2b2f80dc3c3c55f99ea31da78e5a2bd1b1fc0a1b
solocommand <solocommand@71d52551-c826-4352-a584-67b5f1798420>
2011-11-29 23:40:46 +0000

    Updated for 4.3 TOC

---
+++ tags/4.3.0r9-release/localization.lua
@@ -0,0 +1,262 @@
+﻿local L = LibStub("AceLocale-3.0"):GetLocale("Titan", true)
+
+TITAN_SOCIAL_MENU_TEXT = "TitanSocial"
+TITAN_SOCIAL_BUTTON_LABEL = "Social: "
+
+-- default (enUS)
+
+TITAN_SOCIAL_BUTTON_TITLE = "Social: "
+REMOTE_CHAT = "Remote Chat"
+
+TITAN_SOCIAL_TOOLTIP = "Social"
+TITAN_SOCIAL_TOOLTIP_REALID = "RealID Friends Online:";
+TITAN_SOCIAL_TOOLTIP_FRIENDS = "Friends Online:";
+TITAN_SOCIAL_TOOLTIP_GUILD = "Guild Members Online:";
+TITAN_SOCIAL_TOOLTIP_MEM = "Memory Utilization:";
+TITAN_SOCIAL_TOOLTIP_MEM_UNIT = "Kb";
+
+TITAN_SOCIAL_MENU_REALID = "RealID";
+TITAN_SOCIAL_MENU_REALID_FRIENDS = "Show RealID Friends";
+TITAN_SOCIAL_MENU_REALID_BROADCASTS = "Show RealID Broadcasts";
+
+TITAN_SOCIAL_MENU_FRIENDS = "Friends";
+TITAN_SOCIAL_MENU_FRIENDS_SHOW = "Show Friends";
+TITAN_SOCIAL_MENU_FRIENDS_NOTE = "Show Friends Note";
+
+TITAN_SOCIAL_MENU_GUILD = "Guild";
+TITAN_SOCIAL_MENU_GUILD_MEMBERS = "Show Guild Members";
+TITAN_SOCIAL_MENU_GUILD_LABEL = "Show Guild Label";
+TITAN_SOCIAL_MENU_GUILD_NOTE = "Show Guild Note";
+TITAN_SOCIAL_MENU_GUILD_ONOTE = "Show Officer Note";
+
+TITAN_SOCIAL_MENU_LABEL = "Show Label";
+TITAN_SOCIAL_MENU_MEM = "Show Memory Usage";
+TITAN_SOCIAL_MENU_HIDE = "Hide";
+TITAN_SOCIAL_MENU_OPTIONS = "Options";
+
+
+-- frFR
+
+if (GetLocale() == "frFR") then
+
+-- Last Update by Sasmira: 10/30/2010
+
+	TITAN_SOCIAL_BUTTON_TITLE = "Social: "
+
+	TITAN_SOCIAL_TOOLTIP = "Social";
+	TITAN_SOCIAL_TOOLTIP_REALID = "Amis R\195\169els:";
+	TITAN_SOCIAL_TOOLTIP_FRIENDS = "Contacts:";
+	TITAN_SOCIAL_TOOLTIP_GUILD = "Membres de la guilde:";
+	TITAN_SOCIAL_TOOLTIP_MEM = "Mémoire Utilis\195\169e:";
+	TITAN_SOCIAL_TOOLTIP_MEM_UNIT = "Ko";
+	
+	TITAN_SOCIAL_MENU_REALID = "Amis R\195\169els";
+	TITAN_SOCIAL_MENU_REALID_FRIENDS = "Afficher: Amis R\195\169els";
+	TITAN_SOCIAL_MENU_REALID_BROADCASTS = "Afficher: Nombre d'Amis R\195\169els";
+	
+	TITAN_SOCIAL_MENU_FRIENDS = "Contacts";
+	TITAN_SOCIAL_MENU_FRIENDS_SHOW = "Afficher: Contacts";
+	TITAN_SOCIAL_MENU_FRIENDS_NOTE = "Afficher: Notes Contacts";
+		
+	TITAN_SOCIAL_MENU_GUILD = "Guilde";
+	TITAN_SOCIAL_MENU_GUILD_MEMBERS = "Afficher: Membres de Guilde";
+	TITAN_SOCIAL_MENU_GUILD_LABEL = "Afficher: Nom de Guilde";
+	TITAN_SOCIAL_MENU_GUILD_NOTE = "Afficher: Notes de Guilde";
+	TITAN_SOCIAL_MENU_GUILD_ONOTE = "Afficher: Notes d'Officier";
+	
+	TITAN_SOCIAL_MENU_LABEL = "Afficher le titre";
+	TITAN_SOCIAL_MENU_MEM = "Afficher: Mémoire utilis\195\169e";
+	TITAN_SOCIAL_MENU_HIDE = "Cacher";
+	TITAN_SOCIAL_MENU_OPTIONS = "Options";
+	
+end
+
+
+-- deDE
+
+if (GetLocale() == "deDE") then
+
+-- Last Update by solocommand: 10/29/2010
+
+	--TITAN_SOCIAL_BUTTON_TITLE = "";
+
+	--TITAN_SOCIAL_TOOLTIP = "";
+	TITAN_SOCIAL_TOOLTIP_REALID = "Kontakte Freundschaftssystem:";
+	TITAN_SOCIAL_TOOLTIP_FRIENDS = "Kontakte:";
+	--TITAN_SOCIAL_TOOLTIP_GUILD = "Gilde Members:";
+	TITAN_SOCIAL_TOOLTIP_MEM = "Speicher Verbrauch:";
+	TITAN_SOCIAL_TOOLTIP_MEM_UNIT = "KB";
+	
+	TITAN_SOCIAL_MENU_REALID = "Freundschaftssystem";
+	TITAN_SOCIAL_MENU_REALID_FRIENDS = "Ziege Kontakte Freundschaftssystem";
+	TITAN_SOCIAL_MENU_REALID_BROADCASTS = "Ziege Kontakte Statusmeldungen";
+	
+	TITAN_SOCIAL_MENU_FRIENDS = "Kontakte";
+	TITAN_SOCIAL_MENU_FRIENDS_SHOW = "Zeige Kontakte";
+	TITAN_SOCIAL_MENU_FRIENDS_NOTE = "Zeige Notiz Kontakte";
+	
+	TITAN_SOCIAL_MENU_GUILD = "Gilde";
+	--TITAN_SOCIAL_MENU_GUILD_MEMBERS = "Zeige Members Gilde";
+	TITAN_SOCIAL_MENU_GUILD_LABEL = "Ziege Gildename";
+	--TITAN_SOCIAL_MENU_GUILD_NOTE = "Ziege Member Notiz";
+	--TITAN_SOCIAL_MENU_GUILD_ONOTE = "Ziege Officer Notiz";
+	
+	--TITAN_SOCIAL_MENU_LABEL = "Ziege Label";
+	TITAN_SOCIAL_MENU_MEM = "Zeige Speicher Verbrauch";
+	TITAN_SOCIAL_MENU_HIDE = "Ausblenden";
+	TITAN_SOCIAL_MENU_OPTIONS = "Optionen";
+	
+end
+
+
+-- zhTW
+
+if (GetLocale() == "zhTW") then
+
+--TITAN_SOCIAL_BUTTON_TITLE = "Social: "
+
+--TITAN_SOCIAL_TOOLTIP = "Social"
+TITAN_SOCIAL_TOOLTIP_REALID = "在線RealID:";
+TITAN_SOCIAL_TOOLTIP_FRIENDS = "在線好友:";
+TITAN_SOCIAL_TOOLTIP_GUILD = "在線公會成員:";
+TITAN_SOCIAL_TOOLTIP_MEM = "記憶體使用:";
+--TITAN_SOCIAL_TOOLTIP_MEM_UNIT = "Kb";
+
+TITAN_SOCIAL_MENU_REALID = "RealID";
+TITAN_SOCIAL_MENU_REALID_FRIENDS = "顯示RealID";
+TITAN_SOCIAL_MENU_REALID_BROADCASTS = "顯示RealID廣播";
+
+TITAN_SOCIAL_MENU_FRIENDS = "好友";
+TITAN_SOCIAL_MENU_FRIENDS_SHOW = "顯示好友";
+TITAN_SOCIAL_MENU_FRIENDS_NOTE = "顯示好友註記";
+
+TITAN_SOCIAL_MENU_GUILD = "公會";
+TITAN_SOCIAL_MENU_GUILD_MEMBERS = "顯示公會成員";
+TITAN_SOCIAL_MENU_GUILD_LABEL = "顯示公會會階";
+TITAN_SOCIAL_MENU_GUILD_NOTE = "顯示公會註記";
+TITAN_SOCIAL_MENU_GUILD_ONOTE = "顯示公會幹部註記";
+
+TITAN_SOCIAL_MENU_LABEL = "顯示標籤";
+TITAN_SOCIAL_MENU_MEM = "顯示記憶體使用";
+TITAN_SOCIAL_MENU_HIDE = "隱藏";
+TITAN_SOCIAL_MENU_OPTIONS = "選項";
+
+end
+
+-- esES
+
+if (GetLocale() == "esES") then
+
+	--
+	
+end
+
+
+-- ruRU
+
+if (GetLocale() == "ruRU") then
+
+	--
+	
+end
+
+-- CLASSINDEX via TitanGuild
+TITAN_SOCIAL_CLASSINDEX = {
+	--enUS
+	["Druid"]        = 1,
+	["Hunter"]       = 2,
+	["Mage"]         = 3,
+	["Paladin"]      = 4,
+	["Priest"]       = 5,
+	["Rogue"]        = 6,
+	["Shaman"]       = 7,
+	["Warlock"]      = 8,
+	["Warrior"]      = 9,
+	["Death Knight"] = 10,
+
+	-- de
+	["Druide"] = 1,
+	["Druidin"] = 1,
+	["J\195\164ger"] = 2,
+	["J\195\164gerin"] = 2,
+	["Magier"] = 3,
+	["Magierin"] = 3,
+	["Paladin"] = 4,
+	["Priester"] = 5,
+	["Priesterin"] = 5,
+	["Schurke"] = 6,
+	["Schurkin"] = 6,
+	["Schamane"] = 7,
+	["Schamanin"] = 7,
+	["Hexenmeister"] = 8,
+	["Hexenmeisterin"] = 8,
+	["Krieger"] = 9,
+	["Kriegerin"] = 9,
+    ["Todesritter"] = 10,
+
+	-- fr
+	["Druide"] = 1,
+	["Druidesse"] = 1,
+	["Chasseur"] = 2,
+	["Chasseresse"] = 2,
+	["Mage"] = 3,
+	["Paladin"] = 4,
+	["Pr\195\170tre"] = 5,
+	["Pr\195\170tresse"] = 5,
+	["Voleur"] = 6,
+	["Voleuse"] = 6,
+	["Chaman"] = 7,
+	["Chamane"] = 7,
+	["D\195\169moniste"] = 8,
+	["Guerrier"] = 9,
+	["Guerri\195\168re"] = 9,
+    ["Chevalier de la mort"] = 10,
+
+	-- es
+	["Druida"]   = 1,
+	["Cazador"]  = 2,
+	["Cazadora"]  = 2,
+	["Mago"]    = 3,
+	["Maga"]    = 3,
+	["Palad\195\173n"] = 4,
+	["Sacerdote"]  = 5,
+	["Sacerdotisa"]  = 5,
+	["P\195\173caro"]   = 6,
+	["P\195\173cara"]   = 6,
+	["Cham\195\161n"]  = 7,
+	["Brujo"] = 8,
+	["Bruja"] = 8,
+	["Guerrero"] = 9,
+	["Guerrera"] = 9,
+    ["Caballero de la Muerte"] = 10,
+
+	-- ru	
+	["Друид"]   = 1,
+	["Охотник"]  = 2,
+	["Охотница"]  = 2,
+	["Маг"]    = 3,
+	["Паладин"] = 4,
+	["Жрец"]  = 5,
+	["Жрица"]  = 5,
+	["Разбойник"]   = 6,
+	["Разбойница"]   = 6,
+	["Шаман"]  = 7,
+	["Шаманка"]  = 7,
+	["Чернокнижник"] = 8,
+	["Чернокнижница"] = 8,
+	["Воин"] = 9,
+	["Рыцарь смерти"] = 10,
+	
+	-- zhTW
+    ["德魯伊"] = 1,
+    ["獵人"] = 2,
+    ["法師"] = 3,
+    ["聖騎士"] = 4,
+    ["牧師"] = 5,
+    ["盜賊"] = 6,
+    ["薩滿"] = 7,
+    ["術士"] = 8,
+    ["戰士"] = 9,
+    ["死亡騎士"] = 10,
+	
+};
