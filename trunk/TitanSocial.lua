cc2a6277af5d6cb210f7383d839d85fe0d712cf7
eridius <eridius@71d52551-c826-4352-a584-67b5f1798420>
2013-01-14 04:27:29 +0000

    Implement guild member sorting

--- trunk/TitanSocial.lua
+++ trunk/TitanSocial.lua
@@ -91,7 +91,7 @@ function TitanPanelSocialButton_OnLoad(self)
 				--ShowRegularText = false,
 				--ShowColoredText = true,
 			},
-			savedVariables = {       
+			savedVariables = {
 				ShowRealID = 1,
 				ShowRealIDBroadcasts = false,
 				ShowFriends = 1,
@@ -102,6 +102,9 @@ function TitanPanelSocialButton_OnLoad(self)
 				ShowSplitRemoteChat = 1,
 				ShowGuildONote = 1,
 				ShowGroupMembers = 1,
+				SortGuild = false,
+				GuildSortKey = "rank",
+				GuildSortAscending = true,
 				ShowIcon = 1,
 				ShowLabel = 1,
 				ShowTooltipTotals = 1,
@@ -225,183 +228,115 @@ end
 --  TitanPanelRightClickMenu_PrepareSocialMenu()
 ----------------------------------------------------------------------
 
-function TitanPanelRightClickMenu_PrepareSocialMenu()     
-	
-	local info = {};
-	
-	
-	-- Level 2
-	if _G["UIDROPDOWNMENU_MENU_LEVEL"] == 2 then
-	
-		-- RealID Menu
-		if _G["UIDROPDOWNMENU_MENU_VALUE"] == "RealID" then
-			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_REALID, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
+local function addSubmenu(text, value, level)
+	local info = UIDropDownMenu_CreateInfo()
+	info.text = text
+	info.menuList = value
+	info.hasArrow = true
+	info.notCheckable = true
+	info.keepShownOnClick = true
+	UIDropDownMenu_AddButton(info, level)
+end
 
-			-- Show RealID Friends
-				local temptable = {TITAN_SOCIAL_ID, "ShowRealID"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_REALID_FRIENDS;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowRealID");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
-			-- Show RealID Broadcasts
-				local temptable = {TITAN_SOCIAL_ID, "ShowRealIDBroadcasts"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_REALID_BROADCASTS;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowRealIDBroadcasts");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
+local function setGuildSortOption(info, key, value)
+	TitanSetVar(TITAN_SOCIAL_ID, key, value)
+end
+
+local function sortCheckedFunc(button)
+	local current = TitanGetVar(TITAN_SOCIAL_ID, button.arg1)
+	return (current or false) == button.arg2
+end
+
+local function addSortOption(text, key, value, level)
+	local info = UIDropDownMenu_CreateInfo()
+	info.text = text
+	info.func = setGuildSortOption
+	info.arg1 = key
+	info.arg2 = value
+	info.keepShownOnClick = false -- can't update the menu while visible
+	info.checked = sortCheckedFunc
+	info.disabled = not TitanGetVar(TITAN_SOCIAL_ID, "SortGuild")
+	UIDropDownMenu_AddButton(info, level)
+end
+
+function TitanPanelRightClickMenu_PrepareSocialMenu(frame, level, menuList)
+	if level == 1 then
+		TitanPanelRightClickMenu_AddTitle(TitanPlugins[TITAN_SOCIAL_ID].menuText, level);
+		
+		-- RealID Menu
+		addSubmenu(TITAN_SOCIAL_MENU_REALID, "RealID", level)
+		
+		-- Friends Menu
+		addSubmenu(TITAN_SOCIAL_MENU_FRIENDS, "Friends", level)
+		
+		-- Guild Menu
+		addSubmenu(TITAN_SOCIAL_MENU_GUILD, "Guild", level)
+		
+		TitanPanelRightClickMenu_AddSpacer(level);
+		TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_SHOW_GROUP_MEMBERS, TITAN_SOCIAL_ID, "ShowGroupMembers", nil, level)
+		
+		TitanPanelRightClickMenu_AddSpacer(level)
+		TitanPanelRightClickMenu_AddToggleIcon(TITAN_SOCIAL_ID, level)
+		TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_LABEL, TITAN_SOCIAL_ID, "ShowLabel", nil, level)
+		TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_MEM, TITAN_SOCIAL_ID, "ShowMem", nil, level)
+		TitanPanelRightClickMenu_AddSpacer(level)
+		TitanPanelRightClickMenu_AddCommand(TITAN_SOCIAL_MENU_HIDE, TITAN_SOCIAL_ID, TITAN_PANEL_MENU_FUNC_HIDE, level)
+	elseif level == 2 then
+		-- RealID Menu
+		if menuList == "RealID" then
+			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_REALID, level);
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_REALID_FRIENDS, TITAN_SOCIAL_ID, "ShowRealID", nil, level)
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_REALID_BROADCASTS, TITAN_SOCIAL_ID, "ShowRealIDBroadcasts", nil, level)
 		end
 		
 		-- Friends Menu
-		if _G["UIDROPDOWNMENU_MENU_VALUE"] == "Friends" then
-			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_FRIENDS, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-
-			-- Show Friends
-				local temptable = {TITAN_SOCIAL_ID, "ShowFriends"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_FRIENDS_SHOW;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowFriends");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
-			-- Show Friend Notes
-			local temptable = {TITAN_SOCIAL_ID, "ShowFriendsNote"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_FRIENDS_NOTE;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowFriendsNote");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
+		if menuList == "Friends" then
+			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_FRIENDS, level);
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_FRIENDS_SHOW, TITAN_SOCIAL_ID, "ShowFriends", nil, level)
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_FRIENDS_NOTE, TITAN_SOCIAL_ID, "ShowFriendsNote", nil, level)
 		end
 		
 		-- Guild Menu
-		if _G["UIDROPDOWNMENU_MENU_VALUE"] == "Guild" then
-			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_GUILD, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-
-			-- Show Guild Members
-				local temptable = {TITAN_SOCIAL_ID, "ShowGuild"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_GUILD_MEMBERS;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
-			-- Show Guild Name as Label
-				local temptable = {TITAN_SOCIAL_ID, "ShowGuildLabel"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_GUILD_LABEL;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildLabel");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
-			-- Show Guild Note
-				local temptable = {TITAN_SOCIAL_ID, "ShowGuildNote"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_GUILD_NOTE;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildNote");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
-			-- Show Officer Note
-				local temptable = {TITAN_SOCIAL_ID, "ShowGuildONote"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_GUILD_ONOTE;
-				info.value = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildONote");
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
-      -- Show Separate Remote Chat
-				local temptable = {TITAN_SOCIAL_ID, "ShowSplitRemoteChat"};
-				info = {};
-				info.text = TITAN_SOCIAL_MENU_GUILD_REMOTE_CHAT;
-				info.valeu = temptable;
-				info.func = function()
-					TitanPanelRightClickMenu_ToggleVar(temptable);
-					end
-				info.checked = TitanGetVar(TITAN_SOCIAL_ID, "ShowSplitRemoteChat")
-				info.keepShowOnClick = 1;
-				UIDropDownMenu_AddButton(info, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
-				
+		if menuList == "Guild" then
+			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_GUILD, level);
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_GUILD_MEMBERS, TITAN_SOCIAL_ID, "ShowGuild", nil, level)
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_GUILD_LABEL, TITAN_SOCIAL_ID, "ShowGuildLabel", nil, level)
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_GUILD_NOTE, TITAN_SOCIAL_ID, "ShowGuildNote", nil, level)
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_GUILD_ONOTE, TITAN_SOCIAL_ID, "ShowGuildONote", nil, level)
+			TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_GUILD_REMOTE_CHAT, TITAN_SOCIAL_ID, "ShowSplitRemoteChat", nil, level)
+			
+			TitanPanelRightClickMenu_AddSpacer(level)
+			addSubmenu(TITAN_SOCIAL_MENU_GUILD_SORT, "GuildSort", level)
 		end
 		
-		if _G["UIDROPDOWNMENU_MENU_VALUE"] == "Options" then
-			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_OPTIONS, _G["UIDROPDOWNMENU_MENU_LEVEL"]);
+		if menuList == "Options" then
+			TitanPanelRightClickMenu_AddTitle(TITAN_SOCIAL_MENU_OPTIONS, level);
 		end
-		
-		return
+	elseif level == 3 then
+		-- Guild Sorting
+		if menuList == "GuildSort" then
+			-- we'd like to use AddToggleVar() but we can't keep the menu open
+			do
+				local info = UIDropDownMenu_CreateInfo()
+				info.text = TITAN_SOCIAL_MENU_GUILD_SORT_DEFAULT
+				info.func = function ()
+					TitanToggleVar(TITAN_SOCIAL_ID, "SortGuild")
+				end
+				info.keepShownOnClick = false
+				info.checked = not TitanGetVar(TITAN_SOCIAL_ID, "SortGuild")
+				UIDropDownMenu_AddButton(info, level)
+			end
+			TitanPanelRightClickMenu_AddSpacer(level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_NAME, "GuildSortKey", "name", level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_RANK, "GuildSortKey", "rank", level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_CLASS, "GuildSortKey", "class", level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_LEVEL, "GuildSortKey", "level", level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_ZONE, "GuildSortKey", "zone", level)
+			TitanPanelRightClickMenu_AddSpacer(level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_ASCENDING, "GuildSortAscending", true, level)
+			addSortOption(TITAN_SOCIAL_MENU_GUILD_SORT_DESCENDING, "GuildSortAscending", false, level)
+		end
 	end
-  
-  
-	-- Level 1
-	
-	TitanPanelRightClickMenu_AddTitle(TitanPlugins[TITAN_SOCIAL_ID].menuText);
-	
-	-- RealID Menu
-		info={};
-		info.text = TITAN_SOCIAL_MENU_REALID;
-		info.value = "RealID";
-		info.hasArrow = 1;
-		UIDropDownMenu_AddButton(info);
-	
-	-- Friends Menu
-		info = {};
-		info.text = TITAN_SOCIAL_MENU_FRIENDS;
-		info.value = "Friends";
-		info.hasArrow = 1;
-		UIDropDownMenu_AddButton(info);
-		
-	-- Guild Menu
-		info = {};
-		info.text = TITAN_SOCIAL_MENU_GUILD;
-		info.value = "Guild";
-		info.hasArrow = 1;
-		UIDropDownMenu_AddButton(info);
-	
-	TitanPanelRightClickMenu_AddSpacer();
-	TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_SHOW_GROUP_MEMBERS, TITAN_SOCIAL_ID, "ShowGroupMembers");
-	
-	TitanPanelRightClickMenu_AddSpacer();
-	TitanPanelRightClickMenu_AddToggleIcon(TITAN_SOCIAL_ID);
-	TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_LABEL, TITAN_SOCIAL_ID, "ShowLabel");
-	TitanPanelRightClickMenu_AddToggleVar(TITAN_SOCIAL_MENU_MEM, TITAN_SOCIAL_ID, "ShowMem");
-	TitanPanelRightClickMenu_AddSpacer();
-	TitanPanelRightClickMenu_AddCommand(TITAN_SOCIAL_MENU_HIDE, TITAN_SOCIAL_ID, TITAN_PANEL_MENU_FUNC_HIDE);
-
-
-
 end
 
 ----------------------------------------------------------------------
@@ -466,15 +401,96 @@ local function getGroupIndicator(name)
 	return ""
 end
 
+local function ternary(cond, a, b)
+	if cond then
+		return a
+	else
+		return b
+	end
+end
+
+-- collects and sorts the guild roster
+-- PARAMETERS:
+--   split - boolean - whether to split the remote chat
+--   sortKey - string - the key to sort by. nil means no sort
+--   sortAscending - boolean - whether the sort is ascending
+-- RETURNS:
+--   table - array of guild roster indices
+--   number - total guild members
+--   number - online guild members
+--   number - remote guild members
+--
+-- If `split` is true, the online and remote sections of the roster are
+-- sorted independently. If false, they're sorted into the same table.
+-- Every entry in the roster is an index suitable for GetGuildRosterInfo()
+local function collectGuildRosterInfo(split, sortKey, sortAscending)
+	SetGuildRosterShowOffline(false)
+
+	local guildTotal, guildOnline, guildRemote = GetNumGuildMembers()
+
+	local onlineTable, remoteTable = {}, {}
+	local numOnline = split and guildOnline or guildRemote
+	for i = 1, numOnline do
+		onlineTable[i] = i
+	end
+	for i = numOnline+1, guildRemote do
+		remoteTable[i-numOnline] = i
+	end
+	local function tableDesc(t)
+		local desc = "{"
+		for i = 1, #t do
+			if i ~= 1 then desc = desc .. ", " end
+			desc = desc .. (t[i] == nil and "nil" or tostring(t[i]))
+		end
+		return desc.."}"
+	end
+
+	if sortKey then
+		local function sortFunc(a, b)
+			local aname, _, arankIndex, alevel, aclass, azone = GetGuildRosterInfo(a)
+			local bname, _, brankIndex, blevel, bclass, bzone = GetGuildRosterInfo(b)
+			if sortKey == "rank" and arankIndex ~= brankIndex then
+				-- rank indices are reversed from what you'd expect, so flip the meaning of ascending
+				return ternary(sortAscending, arankIndex > brankIndex, arankIndex < brankIndex)
+			end
+			if sortKey == "level" and alevel ~= blevel then
+				return ternary(sortAscending, alevel < blevel, alevel > blevel)
+			end
+			if sortKey == "class" and aclass ~= bclass then
+				return ternary(sortAscending, aclass < bclass, aclass > bclass)
+			end
+			if sortKey == "zone" and azone ~= bzone then
+				return ternary(sortAscending, azone < bzone, azone > bzone)
+			end
+			aname = string.lower(aname)
+			bname = string.lower(bname)
+			-- if name is the secondary sort, it's always ascending
+			if sortAscending or sortKey ~= "name" then
+				return aname < bname
+			else
+				return aname > bname
+			end
+		end
+
+		table.sort(onlineTable, sortFunc)
+		table.sort(remoteTable, sortFunc)
+	end
+
+	-- tack remoteTable onto the end of onlineTable so our caller only has 1 table to traverse
+	for i, v in ipairs(remoteTable) do
+		onlineTable[i+numOnline] = v
+	end
+
+	return onlineTable, guildTotal, guildOnline, guildRemote
+end
+
 local knownLocalRealmID = nil
 
 function TitanPanelSocialButton_GetTooltipText()
 
 	local iRealIDTotal, iRealIDOnline = 0;
 	local iFriendsTotal, iFriendsOnline = 0;
-	local iGuildTotal, iGuildOnline = 0;
 	local tTooltipRichText, playerStatus, clientName = "";
-	local bGuildOffline = GetGuildRosterShowOffline()	-- Enable/disable including offline guild members
 	
 	--
 	--	RealID Friends
@@ -628,25 +644,24 @@ function TitanPanelSocialButton_GetTooltipText()
 	-- Guild
 	--
 	
-	if(TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") ~=nil) then
-	
-		-- Turn off showoffline for tooltip
-		SetGuildRosterShowOffline(false);
-		
-		iGuildTotal, iGuildOnline, iGuildRemote = GetNumGuildMembers();
-		--iGuildOnline   = "|cff00FF00"..iGuildOnline.."|r";
-		
+	if TitanGetVar(TITAN_SOCIAL_ID, "ShowGuild") then
+		local wasOffline = GetGuildRosterShowOffline()
+		SetGuildRosterShowOffline(false)
+
+		local split = TitanGetVar(TITAN_SOCIAL_ID, "ShowSplitRemoteChat")
+		local sortKey = TitanGetVar(TITAN_SOCIAL_ID, "SortGuild") and TitanGetVar(TITAN_SOCIAL_ID, "GuildSortKey") or nil
+		local roster, guildTotal, guildOnline, guildRemote = collectGuildRosterInfo(split, sortKey, TitanGetVar(TITAN_SOCIAL_ID, "GuildSortAscending") or false)
+
 		local remoteChatText = nil
-		local numGuild = iGuildRemote
-		if TitanGetVar(TITAN_SOCIAL_ID, "ShowSplitRemoteChat") ~= nil then
+		local numGuild = guildRemote
+		if split then
 			remoteChatText = ""
-			numGuild = iGuildOnline
+			numGuild = guildOnline
 		end
 		local guildText = ""
 		
-		for guildIndex=1, iGuildRemote do
-		
-			name, rank, rankIndex, level, class, zone, note, officernote, online, playerStatus, classFileName, achievementPoints, achievementRank, isMobile = GetGuildRosterInfo(guildIndex);
+		for _, guildIndex in ipairs(roster) do
+			name, rank, rankIndex, level, class, zone, note, officernote, online, playerStatus, classFileName, achievementPoints, achievementRank, isMobile = GetGuildRosterInfo(guildIndex)
 			
 			local currentText = ""
 
@@ -657,7 +672,7 @@ function TitanPanelSocialButton_GetTooltipText()
 				name = "Unknown"
 			end
 			
-			local isRemote = (guildIndex > iGuildOnline)
+			local isRemote = guildIndex > guildOnline
 			if isMobile then
 				if isRemote then zone = REMOTE_CHAT end
 				if playerStatus == 2 then
@@ -678,31 +693,31 @@ function TitanPanelSocialButton_GetTooltipText()
 			currentText = currentText..TitanPanelSocialButton_ColorText(name, class).." ";
 
 			-- Status
-			if (playerStatus ~= 0) then
-                  if (playerStatus == 1) then
-                    currentText = currentText.."|cffFFFFFF".."<AFK>".."|r  ";
-                  else
-                    currentText = currentText.."|cffFFFFFF".."<DND>".."|r  ";
-                  end
-	           end
+			if playerStatus ~= 0 then
+				if playerStatus == 1 then
+					currentText = currentText.."|cffFFFFFF".."<AFK>".."|r  ";
+				else
+					currentText = currentText.."|cffFFFFFF".."<DND>".."|r  ";
+				end
+			end
 
 			-- Rank
 			currentText = currentText..rank.."  ";
 			
 			-- Notes
-			if(TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildNote") ~= nil) then
+			if TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildNote") then
 				currentText = currentText.."|cffFFFFFF"..note.."|r  "
 			end
 			
 			-- Officer Notes
-			if(TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildONote") ~= nil) then
-				if(CanViewOfficerNote()) then
+			if TitanGetVar(TITAN_SOCIAL_ID, "ShowGuildONote") then
+				if CanViewOfficerNote() then
 					currentText = currentText.."|cffAAFFAA"..officernote.."|r  "
 				end
 			end
 			
 			-- Location
-			if (zone ~= nil) then 
+			if zone ~= nil then 
 				currentText = currentText.."\t|cffFFFFFF"..zone.."|r\n"
 			else
 				currentText = currentText.."\n"
@@ -713,21 +728,16 @@ function TitanPanelSocialButton_GetTooltipText()
 			else
 				guildText = guildText..currentText
 			end
-			
 		end
 		
-		-- Reset ShowOffline Guild Members to original value
-		if (bGuildOffline) then
-			SetGuildRosterShowOffline(true);
-		end
-		
-		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText(TITAN_SOCIAL_TOOLTIP_GUILD).."\t".."|cff00FF00"..numGuild.."|r"..TitanUtils_GetNormalText("/"..iGuildTotal).."\n"..guildText
+		tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText(TITAN_SOCIAL_TOOLTIP_GUILD).."\t".."|cff00FF00"..numGuild.."|r"..TitanUtils_GetNormalText("/"..guildTotal).."\n"..guildText
 
 		if remoteChatText ~= nil then
-			local numRemoteChat = iGuildRemote - iGuildOnline
-			tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText(TITAN_SOCIAL_TOOLTIP_REMOTE_CHAT).."\t".."|cff00FF00"..numRemoteChat.."|r"..TitanUtils_GetNormalText("/"..iGuildTotal).."\n"..remoteChatText
+			local numRemoteChat = guildRemote - guildOnline
+			tTooltipRichText = tTooltipRichText.." \n"..TitanUtils_GetNormalText(TITAN_SOCIAL_TOOLTIP_REMOTE_CHAT).."\t".."|cff00FF00"..numRemoteChat.."|r"..TitanUtils_GetNormalText("/"..guildTotal).."\n"..remoteChatText
 		end
-	
+
+		SetGuildRosterShowOffline(wasOffline)
 	end
 	
 	if (TitanGetVar(TITAN_SOCIAL_ID, "ShowMem") ~=nil) then
@@ -736,43 +746,3 @@ function TitanPanelSocialButton_GetTooltipText()
 	
 	return tTooltipRichText;
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
